28131db67afaafce365a40d8ebf78dedc335b219
Walt ODD <86666367+waltodd@users.noreply.github.com>
2022-11-18 15:46:18 +0100

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import {AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai'
 const Footer = () => {
   return (
     <div className='footer-container'>
-      <p>2022 Hakuna Kelele Store All rights reserverd</p>
+      <p>2022Hakuna Kelele Store Todos os direitos reservados</p>
       <p className='icons'>
         <AiFillInstagram />
         <AiOutlineTwitter />
