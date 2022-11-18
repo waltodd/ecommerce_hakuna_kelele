c9bc5ea2d115db17d24edc3324740a2dba1cff81
Walt ODD <86666367+waltodd@users.noreply.github.com>
2022-11-18 16:03:55 +0100

    Update Product.jsx

--- components/Product.jsx
+++ components/Product.jsx
@@ -10,7 +10,7 @@ const Product = ({product:{image, name, slug, price}}) => {
         <div className='product-card'>
           <img src={urlFor(image && image[0])} width={250} height={250} className="product-image"/>
           <p className='product-name'>{name}</p>
-          <p className='product-price'>${price}</p>
+          <p className='product-price'>${price} Kz</p>
         </div>
       </Link>
     </div>
